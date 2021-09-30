47afe43e1e67b449254eede3cf00ba252dd6c0f7
Momin94 <momin.shoaib@cheetay.pk>
2021-09-30 18:05:18 +0500

    first commit after setting up model class

---
+++ PopularMovies/Services/AppServiceClient.swift
@@ -0,0 +1,35 @@
+import Alamofire
+ 
+// MARK: - AppServerClient
+class AppServerClient {
+ 
+    // MARK: - GetFriends
+    enum GetFriendsFailureReason: Int, Error {
+        case unAuthorized = 401
+        case notFound = 404
+    }
+ 
+    typealias GetFriendsResult = Result<[Friend], GetFriendsFailureReason>
+    typealias GetFriendsCompletion = (_ result: GetFriendsResult) -> Void
+ 
+    func getFriends(completion: @escaping GetFriendsCompletion) {
+        Alamofire.request("http://friendservice.herokuapp.com/listFriends")
+            .validate()
+            .responseJSON { response in
+                switch response.result {
+                case .success:
+                    guard let jsonArray = response.result.value as? [JSON] else {
+                        completion(.failure(nil))
+                        return
+                    }
+                    completion(.success(payload: jsonArray.flatMap { Friend(json: $0 ) }))
+                case .failure(_):
+                    if let statusCode = response.response?.statusCode,
+                        let reason = GetFriendsFailureReason(rawValue: statusCode) {
+                        completion(.failure(reason))
+                    }
+                    completion(.failure(nil))
+                }
+        }
+    }
+}
