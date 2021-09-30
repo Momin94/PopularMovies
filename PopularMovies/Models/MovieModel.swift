076660052da5df4703f826ebbdd80daca1982136
Momin94 <momin.shoaib@cheetay.pk>
2021-09-30 17:51:25 +0500

    first commit of different approach to mvvm movie app

---
+++ PopularMovies/Models/MovieModel.swift
@@ -0,0 +1,8 @@
+//
+//  MovieModel.swift
+//  PopularMovies
+//
+//  Created by momin on 30/09/2021.
+//
+
+import Foundation
---
+++ PopularMovies/View Models/MovieViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MovieViewModel.swift
+//  PopularMovies
+//
+//  Created by momin on 30/09/2021.
+//
+
+import Foundation
